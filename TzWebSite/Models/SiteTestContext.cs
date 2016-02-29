84c22546a58cc9331153900d3273aebfe2932b01
Gardarin <gardarinigor@gmail.com>
2016-02-29 23:33:06 +0200

    first commit

---
+++ TzWebSite/Models/SiteTestContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions; 
+
+namespace TzWebSite.Models
+{
+    public class SiteTestContext:DbContext
+    {
+        public DbSet<SiteTest> SiteTests { get; set; }
+        public DbSet<TestResult> TimeResult { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        } 
+    }
+}
---
+++ TzWebSite/Models/TestResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TzWebSite.Models
+{
+    public class TestResult
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Time { get; set; }
+    }
+}
