e99f50504fd0f3cab57728254ca8fc25b321115e
Gardarin <gardarinigor@gmail.com>
2016-03-01 19:53:56 +0200

    Add control

---
+++ TzWebSite/Controllers/SiteTestController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Net;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+using TzWebSite.Models;
+
+namespace TzWebSite.Controllers
+{
+    public class SiteTestController : Controller
+    {
+        static WorkModel _workModel = new WorkModel();
+        
+        // GET: SiteTest
+        public ActionResult Index()
+        {
+            return View(_workModel);
+        }
+
+        // GET: SiteTest/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SiteTest/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            string curentUrl = collection["Path"];
+            
+            if (!IsValidUrl(curentUrl))
+            {
+                return View();
+            }
+
+            //finding links
+            List<string> urls = GetUrls(collection["Path"]);
+
+            //sites testing
+            List<TestResult> testResults = CreateTestResults(urls);
+
+            curentUrl = UrlValidation(curentUrl);
+            //storage in DB
+            _workModel.CreateSiteTest(collection["Name"], curentUrl);
+            _workModel.AddTestResult(collection["Name"], curentUrl, testResults);
+
+            return RedirectToAction("Index");
+        }
+
+        private List<string> GetUrls(string curentUrl)
+        {
+            string oldUrl = curentUrl;
+            curentUrl = UrlValidation(curentUrl);
+            //get html document
+            HtmlWeb hw = new HtmlWeb();
+            DateTime dStart = DateTime.Now;
+            HtmlDocument document = hw.Load(curentUrl);
+            List<string> urls = new List<string>();
+
+            
+
+            //finding links
+            foreach (HtmlNode link in document.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                string curentLink = "";
+
+                if (IsValidUrl(att.Value))
+                {
+                    try
+                    {
+                        curentLink = CreatAbsoluteUrlFromRelative(oldUrl, att.Value);
+                        urls.Add(curentLink);
+                    }
+                    catch (UriFormatException e)
+                    {
+                        continue;
+                    }
+                }
+
+            }
+
+            return urls;
+        }
+
+        //The site urls testing 
+        private List<TestResult> CreateTestResults(List<string> urls)
+        {
+            List<TestResult> testResults = new List<TestResult>();
+            TestResult tRes;
+            TimeSpan timeSpan = new TimeSpan();
+            DateTime dStart = DateTime.Now;
+            int time = 0;
+            WebRequest req;
+            HtmlWeb hw = new HtmlWeb();
+            
+            foreach (var link in urls)
+            {
+                try
+                {
+                    req = WebRequest.CreateHttp(link);
+                    req.Timeout = 12000;
+                    req.Method = "get";
+
+                    dStart = DateTime.Now;
+                    req.GetResponse();
+
+                    timeSpan = DateTime.Now - dStart;
+                    time = timeSpan.Milliseconds;
+
+                    tRes = new TestResult();
+                    tRes.Url = link;
+                    tRes.Time = "" + time;
+                    testResults.Add(tRes);
+                }
+                catch (NotSupportedException)
+                {
+                    continue;
+                }
+                catch (UriFormatException e)
+                {
+                    continue;
+                }
+                catch (WebException e)
+                {
+                    continue;
+                }
+            }
+            return testResults;
+        }
+
+        //Create absolute baseUrl
+        private string UrlValidation(string url )
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.Relative))
+            {
+                UriBuilder myURI = new UriBuilder("http", url, 80);
+                url = myURI.Uri.AbsoluteUri;
+            }
+
+            return url;
+        }
+
+        //Create absolute url
+        private string CreatAbsoluteUrlFromRelative(string baseUrl,string url)
+        {
+            if (Uri.IsWellFormedUriString(baseUrl, UriKind.Relative))
+            {
+                baseUrl=UrlValidation(baseUrl);
+            }
+
+            if (Uri.IsWellFormedUriString(url, UriKind.Relative))
+            {
+                UriBuilder myURI = new UriBuilder(baseUrl.Remove(baseUrl.Length-1) + url);
+                url = myURI.Uri.AbsoluteUri;
+            }
+
+            return url;
+        }
+
+        private bool IsValidUrl(string url)
+        {
+            return Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute);
+        }
+    }
+}
---
+++ TzWebSite/Models/WorkModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TzWebSite.Models
+{
+    public class WorkModel
+    {
+        public List<SiteTest> _tests;
+        public SiteTestContext _siteContext;
+
+        public WorkModel()
+        {
+            _tests = new List<SiteTest>();
+            _siteContext = new SiteTestContext();
+        }
+
+        public void CreateSiteTest(string name, string path)
+        {
+            SiteTest siteTest = new SiteTest();
+            siteTest.Name = name;
+            siteTest.Path = path;
+            siteTest.TestResults = new List<TestResult>();
+
+            _siteContext.SiteTests.Add(siteTest);
+            _siteContext.SaveChanges();
+        }
+
+        public void AddTestResult(string name, string path, List<TestResult> results)
+        {
+            _siteContext.SiteTests.First(s => s.Name == name && s.Path == path).TestResults = results;
+            _siteContext.SaveChanges();
+        }
+
+        public void DelSiteTest(string name, string path)
+        {
+            _siteContext.SiteTests.Remove(_siteContext.SiteTests.First(s => s.Name == name && s.Path == path));
+            _siteContext.SaveChanges();
+        }
+
+        public List<SiteTest> GetTests()
+        {
+            return _siteContext.SiteTests.Include("TestResults").OrderBy(x => x.Name).ToList();
+        }
+
+        //creating diagram
+        public byte[] GetGraphic(string name, string path)
+        {
+            SiteTest siteTest = _siteContext.SiteTests.Include("TestResults").First(x => x.Name == name && x.Path == path);
+
+            var tests = _siteContext.SiteTests.Include("TestResults").OrderBy(x => x.Name).ToList();
+
+            PictureBox picture=new PictureBox();
+            Bitmap bitmap = new Bitmap(10*15, 200);
+            picture.Image = bitmap;
+            Graphics graphix = Graphics.FromImage(picture.Image);
+            Pen pen = new Pen(Brushes.Red);
+            pen.Width = 15;
+            graphix.Clear(Color.Black);
+            
+            int i=7;
+            int count = 0;
+            foreach (var r in siteTest.TestResults.OrderByDescending(x=>Convert.ToInt32(x.Time)))
+            {
+                count++;
+                
+                int hight = Convert.ToInt32(r.Time)/5;
+                graphix.DrawLine(pen, i, 200, i, 200-hight);
+                graphix.DrawString(""+(count-1), new Font("Arial", 12),Brushes.Black, i-7, 180);
+                i += 15;
+                if (count == 10)
+                {
+                    break;
+                }
+            }
+
+            MemoryStream memoryStream = new MemoryStream();
+            picture.Image.Save(memoryStream, ImageFormat.Bmp);
+            byte[] bitmapRecord = memoryStream.ToArray();
+
+            return bitmapRecord;
+        }
+
+        
+    }
+}
